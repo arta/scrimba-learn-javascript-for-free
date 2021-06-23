41b6c1467909c378a66c90aaa7af56517055d349
Martin Fencl <martin@layeredcode.com>
2021-06-23 17:12:40 -0500

    Show incremented count on the page
    Instead of console log

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -1,10 +1,7 @@
-// initialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
+let countEl = document.getElementById('count-el')
 let count = 0
 
 function increment() {
   count ++
-  console.log(count)
+  countEl.textContent = count
 }
