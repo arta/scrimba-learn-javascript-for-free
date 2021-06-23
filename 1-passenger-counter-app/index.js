8c7e801f3b577dbfacf5b22622e05c6bdd8b676b
Martin Fencl <martin@layeredcode.com>
2021-06-23 16:59:54 -0500

    Stub out increment onclick event listener
    Use console to log out the btn click

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -2,3 +2,6 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
+function increment() {
+  console.log('Increment btn was clicked')
+}
