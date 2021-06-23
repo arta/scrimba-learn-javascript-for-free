9352608659e0a65bcd907073a1fc4dc6f9767651
Martin Fencl <martin@layeredcode.com>
2021-06-23 17:43:01 -0500

    Add save feature pseudo-code

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -1,3 +1,4 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
 let countEl = document.getElementById('count-el')
 let count = 0
 
@@ -7,5 +8,8 @@ function increment() {
 }
 
 function save() {
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  // 3. Render the variable in the saveEl using innerText
+  // NB: Make sure to not delete the existing content of the paragraph
   console.log('Saved new count')
 }
