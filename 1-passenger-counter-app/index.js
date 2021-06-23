5af414e27b428eab5afa514843279d9c076206b3
Martin Fencl <martin@layeredcode.com>
2021-06-23 17:06:42 -0500

    Implement increment()
    Logs out count to console

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -2,6 +2,9 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
+let count = 0
+
 function increment() {
-  console.log('Increment btn was clicked')
+  count += 1
+  console.log(count)
 }
