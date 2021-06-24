9bf0d304c20c6d0b2c4fda34c7edabf3967e0bcf
Martin Fencl <martin@layeredcode.com>
2021-06-24 09:05:56 -0500

    Rename var

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -12,6 +12,6 @@ function save() {
   // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
   // 3. Render the variable in the saveEl using innerText
   // NB: Make sure to not delete the existing content of the paragraph
-  let saveElString = count + ' - '
-  saveEl.textContent += saveElString
+  let countString = count + ' - '
+  saveEl.textContent += countString
 }
