eca49ca103fccb7b67e38cdd377d34fb05d6e892
Martin Fencl <martin@layeredcode.com>
2021-06-24 13:32:27 -0500

    Implement count reset after save

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -1,4 +1,3 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
 let countEl = document.getElementById('count-el')
 let count = 0
 let saveEl = document.getElementById('save-el')
@@ -9,9 +8,12 @@ function increment() {
 }
 
 function save() {
-  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-  // 3. Render the variable in the saveEl using innerText
-  // NB: Make sure to not delete the existing content of the paragraph
   let countString = count + ' - '
   saveEl.textContent += countString
+  resetCount()
+}
+
+function resetCount() {
+  count = 0
+  countEl.textContent = count
 }
