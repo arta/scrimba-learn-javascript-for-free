a7f0e1d766a877e4b38f93519714ee051219fa53
Martin Fencl <martin@layeredcode.com>
2021-06-24 09:01:04 -0500

    Implement save feature
    Does not reset increment (yet)

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -1,6 +1,7 @@
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
 let countEl = document.getElementById('count-el')
 let count = 0
+let saveEl = document.getElementById('save-el')
 
 function increment() {
   count ++
@@ -11,5 +12,6 @@ function save() {
   // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
   // 3. Render the variable in the saveEl using innerText
   // NB: Make sure to not delete the existing content of the paragraph
-  console.log('Saved new count')
+  let saveElString = count + ' - '
+  saveEl.textContent += saveElString
 }
