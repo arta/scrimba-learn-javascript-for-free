58c126e54b0e693fc5df55765f577820bd98a24b
Martin Fencl <martin@layeredcode.com>
2021-06-24 19:12:52 -0500

    Add error msg feature pseudo-code

--- 2-practice-time-1/5-error-message/index.js
+++ 2-practice-time-1/5-error-message/index.js
@@ -1,3 +1,8 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
+
+// add event listener error() to purchase button
+// set var holding error message
+// write error() function displaying error message
+
