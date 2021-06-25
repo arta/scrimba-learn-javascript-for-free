3c65b580a2cb9f486a6d2562e0c0b039ac1cdfc2
Martin Fencl <martin@layeredcode.com>
2021-06-25 12:03:30 -0500

    Use diff cond operator and value just for exercise

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -3,7 +3,7 @@ let secondCard = 2
 
 let sum = firstCard + secondCard
 
-if (sum < 21) {
+if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳")
