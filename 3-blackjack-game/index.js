9c6319245a324effe0d18716174a6e5ccf199a73
Martin Fencl <martin@layeredcode.com>
2021-06-25 13:23:01 -0500

    Add isAlive var

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -2,6 +2,7 @@ let firstCard = 9
 let secondCard = 2
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂")
@@ -10,4 +11,5 @@ if (sum <= 20) {
   hasBlackJack = true
 } else {
   console.log("You're out of the game! 😭")
+  isAlive = false
 }
