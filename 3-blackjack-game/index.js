ca1831c0e2a52de84a58d5f0c76053dde000c3c2
Martin Fencl <martin@layeredcode.com>
2021-06-24 23:15:22 -0500

    Implement basic rules of Blackjack
    Use if..else conditional

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -2,3 +2,11 @@ let firstCard = 9
 let secondCard = 2
 
 let sum = firstCard + secondCard
+
+if (sum < 21) {
+  console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+  console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+  console.log("You're out of the game! 😭")
+}
