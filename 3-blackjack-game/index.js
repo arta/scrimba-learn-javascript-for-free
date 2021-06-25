bb6d555843ab36a82f9c9f388b76312febf614a8
Martin Fencl <martin@layeredcode.com>
2021-06-25 15:17:41 -0500

    Add start button specs

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -5,6 +5,9 @@ let hasBlackJack = false
 let isAlive = true
 let message = ''
 
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
 if (sum <= 20) {
   message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
