b1756d55c67989a9a2597a93a3241662243f1456
Martin Fencl <martin@layeredcode.com>
2021-06-25 16:56:19 -0500

    Add specs for displaying sum

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -5,8 +5,10 @@ let hasBlackJack = false
 let isAlive = true
 let message = ''
 let messageEl = document.getElementById('message-el')
+// 2. Store the sum paragraph in a variable called sumEl
 
 function startGame() {
+  // 3. Render the sum on the page using this format -> "Sum: 14"
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
