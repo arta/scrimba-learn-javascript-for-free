9db0479026f1739a9af94dcaf9f86f6565385336
Martin Fencl <martin@layeredcode.com>
2021-07-30 12:13:11 -0500

    Stub out getRandomCard()
    Returns 5 for now. Will randomize next!
    Also, dev't note: declared functions are hoisted.

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -1,5 +1,5 @@
-let firstCard = 9
-let secondCard = 2
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -8,6 +8,11 @@ let message = ''
 let messageEl = document.getElementById('message-el')
 let sumEl = document.querySelector('#sum-el')
 let cardsEl = document.getElementById('cards-el')
+
+// Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard() {
+  return 5
+}
 
 function startGame() {
   renderGame()
@@ -35,7 +40,7 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 3
+  let card = getRandomCard()
   sum += card
   cards.push(card)
   console.log(cards)
