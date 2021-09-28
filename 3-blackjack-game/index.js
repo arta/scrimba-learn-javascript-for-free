51adab9d095f7bba6b7d7769dfd89aaa15b80691
Martin Fencl <martin@layeredcode.com>
2021-09-28 16:36:46 -0500

    Constrain dealing of new card
    Deal only if alive or no black jack (essentially, if less than 21)

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -54,9 +54,11 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard()
-  sum += card
-  cards.push(card)
-  console.log(cards)
-  renderGame()
+  if (isAlive && !hasBlackJack) {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+  }
 }
