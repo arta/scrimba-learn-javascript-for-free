f91c2bc69665d7abbd38c426b9e0d1ecfa948294
Martin Fencl <martin@layeredcode.com>
2022-01-10 14:07:18 -0600

    Create player object to store show name & chips

--- 3-blackjack-game/index.js
+++ 3-blackjack-game/index.js
@@ -1,3 +1,7 @@
+let player = {
+  name: 'Arta',
+  chips: 200
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,6 +10,9 @@ let message = ''
 let messageEl = document.getElementById('message-el')
 let sumEl = document.querySelector('#sum-el')
 let cardsEl = document.getElementById('cards-el')
+let playerEl = document.getElementById('player-el')
+
+playerEl.textContent = `${player.name}: ${player.chips}`
 
 // Return random Blackjack cards values
 // if 1           return 11
