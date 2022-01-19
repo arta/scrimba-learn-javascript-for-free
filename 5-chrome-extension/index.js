d13d581fb7c9145e892fda8c4817054a9e01c295
Martin Fencl <martin@layeredcode.com>
2022-01-19 17:55:45 -0600

    Create inputEl and myLeads variables

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -1,13 +1,13 @@
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
 const inputBtn = document.getElementById('input-btn')
+const inputEl = document.getElementById('input-el')
+const myLeads = []
 
 function saveLead() {
   console.log('Button clicked!')
 }
 
 inputBtn.addEventListener('click', saveLead)
-
-inputBtn.addEventListener('click', () => console.log('Button clicked!'))
-
-inputBtn.addEventListener('click', function() {
-  console.log('Button clicked!')
-})
