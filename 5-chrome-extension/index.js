2162792c1f2bb7f5108a13c8492c0c280856aa7a
Martin Fencl <martin@layeredcode.com>
2022-01-19 20:42:40 -0600

    Push the value of the input field

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -2,11 +2,11 @@ const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
 const myLeads = []
 
-// Push the value "www.awesomelead.com" to myLeads
-// when the input button is clicked
-
 function saveLead() {
-  myLeads.push('www.awesomelead.com')
+  // Push the value from the inputEl into the myLeads array
+  // instead of the hard-coded "www.awesomeleads.com" value
+  // Google -> "get value from input field javascript"
+  myLeads.push(inputEl.value)
   console.log(myLeads)
 }
 
