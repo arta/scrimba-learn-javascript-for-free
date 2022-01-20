1737b9b75b712ced342d1db3d594a9de6045ba32
Martin Fencl <martin@layeredcode.com>
2022-01-20 10:41:08 -0600

    Use a for loop to log out myLeads

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -1,13 +1,16 @@
 const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
-const myLeads = []
+const myLeads = ['www.awesomelead.com', 'www.epiclead.com', 'www.greatlead.com']
 
 function saveLead() {
-  // Push the value from the inputEl into the myLeads array
-  // instead of the hard-coded "www.awesomeleads.com" value
-  // Google -> "get value from input field javascript"
   myLeads.push(inputEl.value)
   console.log(myLeads)
 }
 
 inputBtn.addEventListener('click', saveLead)
+
+// Log out the items in the myLeads array using a for loop
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i])
+}
