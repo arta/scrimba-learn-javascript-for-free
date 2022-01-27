ae6820a6627db7ff007dacba5620c2f5419e1cfb
Martin Fencl <martin@layeredcode.com>
2022-01-27 14:16:50 -0600

    Improve the performance of the app
    DOM manipulation has a cost => minimize the count of DOM manipulations.

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -10,15 +10,13 @@ function saveLead() {
 
 inputBtn.addEventListener('click', saveLead)
 
-// Let's try a different method!
-// Use createElement() and append() instead of innerHTML
-for (let i = 0; i < myLeads.length; i++) {
-  // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+let listItems = ''
 
-  // create li element
-  // insert lead
-  // insert element into ul element
-  const liEl = document.createElement('li')
-  liEl.textContent = myLeads[i]
-  ulEl.append(liEl)
+for (let i = 0; i < myLeads.length; i++) {
+  // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+  listItems += `<li>${myLeads[i]}</li>`
 }
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems
