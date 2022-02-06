6d4583a2fc0bd7aa23d644ef1af398e200072366
Martin Fencl <martin@layeredcode.com>
2022-02-06 15:49:04 -0600

    Make the delete button work

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -2,6 +2,8 @@ const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
 let myLeads = []
 const ulEl = document.getElementById('ul-el')
+// 1. Store the delete button in a deleteBtn variable
+const deleteBtn = document.getElementById('delete-btn')
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 
@@ -16,6 +18,14 @@ function saveLead() {
   inputEl.value = null
   renderLeads()
 }
+
+// 2. Listen for double clicks on the delete button (google it!)
+// 3. When clicked, clear localStorage, myLeads, and the DOM
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear()
+  myLeads.length = 0
+  renderLeads() // instead of ulEl.innerHTML = ''
+})
 
 inputBtn.addEventListener('click', saveLead)
 
