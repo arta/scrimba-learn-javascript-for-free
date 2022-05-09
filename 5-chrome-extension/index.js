89382ac37dddb0b28145d108768fec1a66c55d57
Martin Fencl <martin@layeredcode.com>
2022-05-09 10:36:26 -0500

    Tidy up

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -1,42 +1,16 @@
+const deleteBtn = document.getElementById('delete-btn')
 const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
-let myLeads = []
-const ulEl = document.getElementById('ul-el')
-const deleteBtn = document.getElementById('delete-btn')
-const tabBtn = document.getElementById('tab-btn')
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
-
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  renderLinks(myLeads)
-}
-
-function saveLead() {
-  myLeads.push(inputEl.value)
-  localStorage.setItem('myLeads', JSON.stringify(myLeads))
-  inputEl.value = null
-  renderLinks(myLeads)
-}
+const tabBtn = document.getElementById('tab-btn')
+const ulEl = document.getElementById('ul-el')
+let myLeads = []
 
 function deleteAllLeads() {
   localStorage.clear()
   myLeads.length = 0
   renderLinks(myLeads) // instead of ulEl.innerHTML = ''
 }
-
-inputBtn.addEventListener('click', saveLead)
-
-tabBtn.addEventListener('click', function() {
-  // Use Chrome API to get the current window active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    renderLinks(myLeads)
-  })
-})
-
-deleteBtn.addEventListener('dblclick', deleteAllLeads)
 
 function renderLinks(leads) {
   let listItems = ''
@@ -50,4 +24,29 @@ function renderLinks(leads) {
     `
   }
   ulEl.innerHTML = listItems
+}
+
+function saveLead() {
+  myLeads.push(inputEl.value)
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  inputEl.value = null
+  renderLinks(myLeads)
+}
+
+deleteBtn.addEventListener('dblclick', deleteAllLeads)
+
+inputBtn.addEventListener('click', saveLead)
+
+tabBtn.addEventListener('click', function() {
+  // Use Chrome API to get the current window active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    renderLinks(myLeads)
+  })
+})
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLinks(myLeads)
 }
