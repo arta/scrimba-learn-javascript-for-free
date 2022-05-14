d603387d37e048acf50d0d368741deaed999ac72
Martin Fencl <martin@layeredcode.com>
2022-05-14 10:15:25 -0500

    Add js pseudocode
    A draft, to start with

---
+++ 6-invoice-creator/index.js
@@ -0,0 +1,11 @@
+// create container of available services & prices
+// grab add service buttons into variables
+// grab invoice body into variable
+// grab send invoice button into variable
+// listen for add service button click to add its service
+// write function that adds the service to invoice
+// update & render invoice lines
+// update & render invoice total (also render the note)
+// disallow adding a service more than once
+// listen for send invoice button click to "send" (reset)
+// write function that "sends" (resets) the invoice
