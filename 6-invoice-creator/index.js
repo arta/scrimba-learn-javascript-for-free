63ddfcfec079a1862c20959944795667f0b7385c
Martin Fencl <martin@layeredcode.com>
2022-05-15 14:36:11 -0500

    Refactor rendering of invoice into a helper
    Render by looping over an array that contains selected services
    
    Prep for removal, and re-render, of individual invoice lines

--- 6-invoice-creator/index.js
+++ 6-invoice-creator/index.js
@@ -9,6 +9,7 @@
 // disallow adding a service more than once
 // listen for send invoice button click to "send" (reset)
 // write function that "sends" (resets) the invoice
+// implement removal of individual services already added
 
 const get = (elemementId) => document.getElementById(elemementId)
 
@@ -27,22 +28,31 @@ const services = [
   { id: 2, name: 'Pull Weeds', price: 30 }
 ]
 const serviceIdsAdded = []
-
+const servicesAdded = []
 let total = 0
 
 function addService(service) {
   if (!serviceIdsAdded.includes(service.id)) {
-    invoiceLines.innerHTML += `
-      <p class="p-invoice-line">
-        <span>${service.name}</span>
-        <span class="span-price"><span>$</span>${service.price}</span>
-      </p>
-    `
     serviceIdsAdded.push(service.id)
-    spanNotes.textContent = 'We accept cash, credit cards, or PayPal'
-    total += service.price
-    spanTotal.textContent = `$${total}`
+    servicesAdded.push(services[service.id])
+    total += services[service.id].price
   }
+  renderInvoice()
+}
+
+function renderInvoice() {
+  let invoiceLinesStr = ''
+  for (let i = 0; i < servicesAdded.length; i++) {
+    invoiceLinesStr += `
+    <p class="p-invoice-line">
+      <span>${servicesAdded[i].name}</span>
+      <span class="span-price"><span>$</span>${servicesAdded[i].price}</span>
+    </p>
+  `
+  }
+  invoiceLines.innerHTML = invoiceLinesStr
+  spanNotes.textContent = 'We accept cash, credit cards, or PayPal'
+  spanTotal.textContent = `$${total}`
 }
 
 function sendInvoice() {
@@ -50,6 +60,7 @@ function sendInvoice() {
   spanNotes.textContent = ''
   total = 0
   serviceIdsAdded.length = 0
+  servicesAdded.length = 0
   spanTotal.textContent = `$${0}`
 }
 
