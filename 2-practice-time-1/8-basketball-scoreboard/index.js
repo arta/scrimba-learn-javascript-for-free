841b6259ef103c5471c72a9f6f367ab3384197bc
Martin Fencl <martin@layeredcode.com>
2023-02-20 12:03:50 -0600

    Add a Reset button

--- 2-practice-time-1/8-basketball-scoreboard/index.js
+++ 2-practice-time-1/8-basketball-scoreboard/index.js
@@ -2,6 +2,12 @@ let homeScore = 0
 let guestScore = 0
 const displayHome = document.getElementById('display-home')
 const displayGuest = document.getElementById('display-guest')
+const resetScores = document.getElementById('btn-reset')
+
+function reset() {
+  increaseHomeScoreBy(homeScore * (-1))
+  increaseGuestScoreBy(guestScore * (-1))
+}
 
 function score(team, points) {
   if (team == 'home') increaseHomeScoreBy(points)
